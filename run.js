19c17f18bcecf138c6b2208bcb86e07b4c130966
Marcin Zajkowski <marcin.a.zajkowski@gmail.com>
2019-03-16 02:34:13 -0400

    install cache buster

--- run.js
+++ run.js
@@ -1,10 +1,12 @@
 //
 console.log('replace begin');
+const childProcess = require('child_process');
+const gitRevision = childProcess.execSync('git rev-parse HEAD').toString().trim().substr(0, 7);
 const replace = require('replace-in-file');
 const options = {
     files: 'index.html',
     from: '<!---commitId--->',
-    to: 'bar',
+    to: gitRevision,
 };
 replace.sync(options);
 console.log('replace complete');
